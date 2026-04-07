4e777dfc1fd289cf149628df2b8518f10d80cde0
Frank Koomson <frankkoomson1@gmail.com>
2026-04-07 10:45:04 +0000

    Update API URLs to production backend

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const baseURL = "http://localhost:6060/api"
+const baseURL = "https://swk-backend.onrender.com/api"
 
 export const apiClient = axios.create({
     baseURL: baseURL,
